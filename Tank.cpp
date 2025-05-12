8e0af7b06a7a7767b4fa0b01608f1c7384230a9f
Никита <nikitaershov64@gmail.com>
2025-05-12 23:10:39 +0300

    Update Tank.cpp

--- Tank.cpp
+++ Tank.cpp
@@ -1,6 +1,6 @@
 #include "Tank.h"
 #include "Block.h"
-#include <iostream>  // Для отладки
+#include <iostream>
 
 using namespace std;
 
@@ -9,6 +9,8 @@ Tank::Tank() : health(100), damage(10), speed (5){
     position.x1 = position.y1 = -100; // Начальная позиция вне экрана
     position.x2 = position.y2 = -100 + 16; // Начальная позиция вне экрана
     tempPosition = position;
+    blockPositionX = 0; //заготовка
+    blockPositionY = 0;
 }
 
 void Tank::movement(Block Map[HEIGHT][WIDTH]) {
@@ -36,13 +38,37 @@ void Tank::movement(Block Map[HEIGHT][WIDTH]) {
     }
 }
 
-//ToDo
 bool Tank::checkMove(Position pos, Block Map[HEIGHT][WIDTH]) {
+    // Проверяем все 4 угла танка + центр
+    const pair<int, int> checkPoints[] = {
+        {pos.x1, pos.y1},     // левый верхний угол
+        {pos.x2, pos.y1},     // правый верхний угол
+        {pos.x1, pos.y2},     // левый нижний угол
+        {pos.x2, pos.y2},     // правый нижний угол
+        {(pos.x1 + pos.x2) / 2, (pos.y1 + pos.y2) / 2}  // центр
+    };
 
-    if (Map[pos.y1 / 16 + 1][pos.x1 / 16 + 1].blockNumber != Block::emptiness && Map[pos.y1 / 16 + 1][pos.x1 / 16 + 1].blockNumber != Block::foliage)
-        return true;
+    for (const auto& point : checkPoints) {
+        int x = point.first;
+        int y = point.second;
 
-    return false;
+        // Переводим координаты в блоки
+        int blockX = x / 16;
+        int blockY = y / 16;
+
+        // Проверяем границы массива
+        if (blockX < 0 || blockX >= WIDTH || blockY < 0 || blockY >= HEIGHT) {
+            return true; // Считаем выход за границы непроходимым
+        }
+
+        // Проверяем тип блока
+        int blockType = Map[blockY][blockX].blockNumber;
+        if (blockType != Block::emptiness && blockType != Block::foliage) {
+            return true; // Столкновение с непроходимым блоком
+        }
+    }
+
+    return false; // Все проверяемые точки проходимы
 }
 
 void Tank::shoot() {
@@ -50,6 +76,7 @@ void Tank::shoot() {
 }
 
 void Tank::changeOfDirection() {
+
 }
 
 void Tank::treatment() {
