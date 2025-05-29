4349b8ae4dff949108522b28ab4eaddad8ea916a
Никита <nikitaershov64@gmail.com>
2025-05-30 00:24:05 +0300

    Update Tank.h

--- Tank.h
+++ Tank.h
@@ -1,12 +1,15 @@
+#pragma once
 #include <iostream>
-#include "Block.h"
+#include <vector>
+
+class Block;
+class Bullet;
+
 class Tank {
 public:
-
     struct Position {
         int y1;
         int x1;
-
         int y2;
         int x2;
     };
@@ -28,17 +31,32 @@ public:
     bool moveLeft;
     bool moveRight;
 
-    void movement(Block Map[HEIGHT][WIDTH]);
-    void shoot();
+    void movement(Block Map[HEIGHT][WIDTH], std::vector<Tank>& tanks); // Добавлено: принимает вектор танков
+    void Shoot(Block Map[HEIGHT][WIDTH]);
+    bool damageThis(int damage);
+    std::vector<Bullet>& GetBullets();
+
+    void SetAngle(int angle);
+    int GetAngle() const;
+    int GetTempAngle() const;
+
     void changeOfDirection();
     void treatment();
+
     bool checkMove(Position pos, Block Map[HEIGHT][WIDTH]);
+    bool isColliding(Position pos1, Position pos2) const;
 
-    Tank();  // Объявление конструктора
+    Tank();
 
+    int GetHealth() const;
+    void SetHealth(int newHealth);
+    bool isAlive();
 protected:
-
+    std::vector<Bullet> bullets;
+    int angle;
+    int tempAngle;
     int speed;
     int health;
     int damage;
+    bool live;
 };
