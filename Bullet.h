a349585d8904f2fa0b944c1ec217faf1e3bdcb37
Никита <nikitaershov64@gmail.com>
2025-06-01 17:34:30 +0300

    Update Bullet.h

--- Bullet.h
+++ Bullet.h
@@ -1,38 +1,44 @@
 #pragma once
 #include <Windows.h>
-
-class Block; // Forward declaration
-class Tank;
+#include <string>
 #include <vector>
+
+class Block;
+class Tank;
 
 class Bullet {
 public:
     Bullet(int x, int y, int angle, int speed);
     ~Bullet();
 
-    // Добавляем move-семантику
+    // Удаляем конструктор копирования
+    Bullet(const Bullet&) = delete;
+    Bullet& operator=(const Bullet&) = delete;
+
+    // Move-семантика
     Bullet(Bullet&& other) noexcept;
     Bullet& operator=(Bullet&& other) noexcept;
 
     static const int WIDTH = 30;
     static const int HEIGHT = 32;
 
-    void Move(Block Map[HEIGHT][WIDTH], HWND hwnd, std::vector<Tank>& tanks); // Изменено: принимает вектор танков
+    void Move(Block Map[HEIGHT][WIDTH], HWND hwnd, std::vector<Tank>& tanks);
     bool IsAlive();
     void Draw(HDC hdc);
     RECT GetRect() const;
     void togleIsAlive(bool alive);
-    bool isColliding(RECT rect1, RECT rect2) const; // Функция для проверки столкновений
-
-private:
-
-    static const int maxLifetime = 10000;
-    static const int BULLET_SIZE = 4;
+    bool isColliding(RECT rect1, RECT rect2) const;
+    void clearDraw(HDC hdc);
+    void DrawWinMessage(HWND hwnd, const std::wstring& message);
 
     int x;
     int y;
     int tempX;
     int tempY;
+
+private:
+    static const int maxLifetime = 10000;
+    static const int BULLET_SIZE = 4;
 
     int angle;
     int speed;
