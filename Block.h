f0530771a1cdc4a16750b9151a9104c741504977
Никита <nikitaershov64@gmail.com>
2025-06-05 22:20:40 +0300

    Update Block.h

--- Block.h
+++ Block.h
@@ -4,7 +4,8 @@ class Block{
 public:
 	Block();
 
-	void damage(int damage, HWND hwnd);
+	void damage(int damageAmount, HWND hwnd);
+	bool isDestroyed() const;
 	void initialization(int type);
 	void positionBlock(int i, int j);
 	int getNumberBlock();
@@ -15,6 +16,9 @@ public:
 	static const int whiteBrick = 3;
 	static const int foliage = 4;
 	static const int base = 5;
+	static const int healthBox = 6;
+	static const int damageUpBox = 7;
+	static const int speedUpBox = 8;
 
 	struct position {
 		int x1;
@@ -27,5 +31,6 @@ public:
 private:
 	int blockNumber;
 	int endurance;
+	bool destroyed;
 };
 
