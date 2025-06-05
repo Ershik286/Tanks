01954bbcf30142780d5b87698ddfccdc82e7b33a
Никита <nikitaershov64@gmail.com>
2025-06-05 22:19:57 +0300

    Update Block.cpp

--- Block.cpp
+++ Block.cpp
@@ -4,60 +4,63 @@
 
 using namespace std;
 
-Block::Block(){}
+Block::Block(){
+	this->endurance = 1;
+	this->blockNumber = 0;
+	this->destroyed = false;
+	Position = {0, 0, 0, 0};
+}
 
 void Block::initialization(int type){
 	this->blockNumber = type;
 	switch (blockNumber)
 	{
-	case border:
+	case border || whiteBrick || foliage || emptiness:
 		this->endurance = INT_MAX;
 		break;
 	case brick:
 		this->endurance = 30;
 		break;
-	case whiteBrick:
-		this->endurance = INT_MAX;
-		break;
-	case foliage:
-		this->endurance = INT_MAX;
-		break;
 	case base:
 		this->endurance = 250;
-		break;
-	case emptiness:
-		this->endurance = INT_MAX;
 		break;
 	default:
 		break;
 	}
+	if (blockNumber == healthBox || blockNumber == damageUpBox || blockNumber == speedUpBox) {
+		this->endurance = 1;
+	}
 }
 
 void Block::positionBlock(int i, int j) {
-	Position.x1 = j * 16;
-	Position.y1 = i * 16;
-	Position.x2 = (j + 1) * 16;
-	Position.y2 = (i + 1) * 16;
+	this->Position.x1 = j * 16;
+	this->Position.y1 = i * 16;
+	this->Position.x2 = (j + 1) * 16;
+	this->Position.y2 = (i + 1) * 16;
 }
 
 int Block::getNumberBlock(){
 	return blockNumber;
 }
 
-void Block::damage(int damage, HWND hwnd) {
-	if (blockNumber == brick || blockNumber == base) {
-		endurance -= damage;
-		if (endurance <= 0) {
-			this->blockNumber = emptiness;
-			endurance = INT_MAX;
-			RECT areaBlockNull;
-			areaBlockNull.left = Position.x1;
-			areaBlockNull.right = Position.x2;
-			areaBlockNull.top = Position.y1;
-			areaBlockNull.bottom = Position.y2;
-			InvalidateRect(hwnd, &areaBlockNull, TRUE);
-		}
+void Block::damage(int damageAmount, HWND hwnd) {
+	this->endurance -= damageAmount; // reduce the endurance
+
+	// When an object is dead, then it will perform destruction
+	if (endurance <= 0) {
+		initialization(emptiness);
+
+		// Now invalidate the previous rectangle, so that it can be redrawn
+		RECT areaBlockNull;
+		areaBlockNull.left = Position.x1;
+		areaBlockNull.right = Position.x2;
+		areaBlockNull.top = Position.y1;
+		areaBlockNull.bottom = Position.y2;
+
+		InvalidateRect(hwnd, &areaBlockNull, FALSE); // Re-draw the block
 	}
 }
 
-
+bool Block::isDestroyed() const {
+	return destroyed;
+}
