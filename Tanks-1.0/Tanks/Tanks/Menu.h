4a0134f09a4fe9f2cc466ed5874cff9fbb31e856
Никита <nikitaershov64@gmail.com>
2025-11-17 18:28:05 +0300

    Update Menu.h

--- Tanks-1.0/Tanks/Tanks/Menu.h
+++ Tanks-1.0/Tanks/Tanks/Menu.h
@@ -1,15 +1,23 @@
+// Menu.h
 #pragma once
 #ifndef MENU_H
 #define MENU_H
 
 #include <Windows.h>
 
-// Идентификаторы кнопок
+#ifdef MENU_EXPORTS
+#define MENU_API __declspec(dllexport)
+#else
+#define MENU_API __declspec(dllimport)
+#endif
+
 #define ID_BUTTON_PLAY 1001
 #define ID_BUTTON_SETTINGS 1002
 #define ID_BUTTON_EXIT 1003
 
-// Процедура окна меню
-LRESULT CALLBACK WindowProcMenu(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+extern "C" {
+    MENU_API HWND CreateMenuWindow(HINSTANCE hInstance, HWND hParent, HWND hGameWindow, HWND hSettingsWindow);
+    MENU_API LRESULT CALLBACK WindowProcMenu(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+}
 
 #endif
